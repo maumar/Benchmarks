cf5c3be629a928f47eb3f27f73ca2ef012b7890c
maumar <maumar@microsoft.com>
2025-02-06 16:54:58 +0100

    gf

--- src/BenchmarksApps/EFCoreAotQueriesBenchmarks3/Startup.cs
+++ src/BenchmarksApps/EFCoreAotQueriesBenchmarks3/Startup.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreAotQueriesBenchmarks3
 {
